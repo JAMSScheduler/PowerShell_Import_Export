33bf15ac44326d636e16a25c8ca6f15dff652e79
TFinnerty22 <timf@mvpsi.com>
2017-09-15 12:12:09 -0400

    Update ImportScript.ps1

--- ImportScript.ps1
+++ ImportScript.ps1
@@ -1,3 +1,5 @@
 ﻿Import-Module JAMS
 
-Import-JAMSXml -Path "The path to your import file." -TargetFolder "Your folder path in JAMS goes here" -Server "Your server goes here"
+Import-JAMSXml -Path "The path to your first import file." -TargetFolder "Your folder path in JAMS goes here" -Server "Your server goes here"
+
+Import-JAMSXml -Path "The path to your second import file." -TargetFolder "Your folder path in JAMS goes here" -Server "Your server goes here"
